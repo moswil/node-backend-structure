bc707339c9f20c717a699d677253f3f05540a959
moswil <moseswillfred1@gmail.com>
2020-02-08 15:36:33 +0300

    created a register/login API for nodejs using expres

---
+++ models/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.set('debug', true);
+mongoose.Promise = global.Promise;
+mongoose
+	.connect(process.env.DB_URL, {
+		useNewUrlParser: true,
+	})
+	.then(() => console.log('MongoDB connected successfully'))
+	.catch(err => console.log(`ERROR: ${err}`));
+
+module.exports.User = require('./user');
